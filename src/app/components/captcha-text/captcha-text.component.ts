a16baa73eee2bfc91102fd913c56918b9f74ea31
Kurban Ramazanov <kurban.ramazanovv@gmail.com>
2025-07-11 23:08:00 +0300

    captcha-text if incorrect

--- src/app/components/captcha-text/captcha-text.component.ts
+++ src/app/components/captcha-text/captcha-text.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '../../shared/button/buttonGo.component';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-captcha-text',
   standalone: true,
-  imports: [ButtonComponent, FormsModule],
+  imports: [ButtonComponent, FormsModule, CommonModule],
   templateUrl: './captcha-text.component.html',
 })
 export class CaptchaTextComponent {
@@ -14,11 +15,14 @@ export class CaptchaTextComponent {
 
   input = '';
 
+  incorrect = false;
+
   finish() {
     if (this.input === 'e3TJ6Jdp') {
       this.router.navigate(['/result']);
     } else {
-      alert('nah');
+      this.incorrect = true;
+      return;
     }
   }
 }
