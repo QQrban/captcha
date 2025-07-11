36a99ccaf5dc4c122cd6b542f54b25cb368b333a
Kurban Ramazanov <kurban.ramazanovv@gmail.com>
2025-07-11 23:03:37 +0300

    captcha text & result

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,30 @@ import { HomeComponent } from './pages/home/home.component';
 import { CaptchaComponent } from './pages/captcha/captcha.component';
 import { inject } from '@angular/core';
 import { StepService } from './services/step.service';
+import { ResultComponent } from './pages/result/result.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   {
     path: 'captcha',
     component: CaptchaComponent,
+    canActivate: [
+      () => {
+        const step = inject(StepService);
+        const router = inject(Router);
+
+        if (step.step() < 1) {
+          router.navigateByUrl('/');
+          return false;
+        }
+
+        return true;
+      },
+    ],
+  },
+  {
+    path: 'result',
+    component: ResultComponent,
     canActivate: [
       () => {
         const step = inject(StepService);
--- src/app/components/captcha-text/captcha-text.component.ts
+++ src/app/components/captcha-text/captcha-text.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../shared/button/buttonGo.component';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-captcha-text',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, FormsModule],
   templateUrl: './captcha-text.component.html',
 })
 export class CaptchaTextComponent {
+  constructor(private router: Router) {}
 
+  input = '';
+
+  finish() {
+    if (this.input === 'e3TJ6Jdp') {
+      this.router.navigate(['/result']);
+    } else {
+      alert('nah');
+    }
+  }
 }
